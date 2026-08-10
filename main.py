b1d1d5e83c9e6876a89d0772c53a3114348e1238
Astrid Philippet <astrid.philippet@mail.mcgill.ca>
2026-08-10 11:57:46 -0400

    Update portfolio title

---
+++ main.py
@@ -0,0 +1,13 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv(".env")
+
+api_key = os.getenv("API_KEY")
+db_url = os.getenv("DATABASE_URL")
+
+# DO NOT hard-code secrets like this:
+# api_key = "supersecret123"
+
+print(api_key)
+print(db_url)
